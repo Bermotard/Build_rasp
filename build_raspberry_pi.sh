1ed2300eb82ad6fd5cbb5fc190147c847d736729
Bernard <bgosset@gmail.com>
2020-09-17 14:49:56 +0200

    chage build_raspbeery_pi.sh to take gitub repo instead of my repo in my NAS.

--- build_raspberry_pi.sh
+++ build_raspberry_pi.sh
@@ -3,7 +3,7 @@ cd ~/
 rm -Rf pokyzeus
 git clone -b zeus git://git.yoctoproject.org/poky pokyzeus
 cd pokyzeus
-git clone -b develop ssh://bernard@192.168.0.25:8723/volume1/rpi3-build
+git clone -b develop https://github.com/Bermotard/Build_rasp.git rpi3-build
 git clone -b zeus  git://git.yoctoproject.org/meta-raspberrypi
 . ./oe-init-build-env rpi3-build
 bitbake  core-image-minimal
