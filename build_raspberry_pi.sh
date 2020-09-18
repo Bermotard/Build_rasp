65e2a84cc77750094ac6fcf42b3d4c3af7917f52
Bermotard <bgosset83@gmail.com>
2020-09-18 17:35:56 +0000

    Modifiy build_raspberry_pi.sh to clone into rpi3_build

--- build_raspberry_pi.sh
+++ build_raspberry_pi.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-cd ~/
 rm -Rf pokyzeus
 git clone -b zeus git://git.yoctoproject.org/poky pokyzeus
 cd pokyzeus
-git clone -b develop https://github.com/Bermotard/Build_rasp.git rpi3-build
+git clone git@github.com:Bermotard/Build_rasp.git -b develop rpi3_build
 git clone -b zeus  git://git.yoctoproject.org/meta-raspberrypi
 . ./oe-init-build-env rpi3-build
 bitbake  core-image-minimal
