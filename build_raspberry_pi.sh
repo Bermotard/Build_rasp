e1790c380b6ae6112e4accb90de09689126915ae
BernardUbuntu <bgosset83@gmail.com>
2020-09-23 13:19:35 +0200

    clone extra Yocto repo

--- build_raspberry_pi.sh
+++ build_raspberry_pi.sh
@@ -4,6 +4,13 @@ rm -Rf pokyzeus
 git clone -b zeus git://git.yoctoproject.org/poky pokyzeus
 cd pokyzeus
 git clone git@github.com:Bermotard/Build_rasp.git -b develop rpi3_build
+# Meta-openembedded
+git clone -b zeus git://git.openembedded.org/meta-openembedded
+# 96boards
+git clone -b zeus git://github.com/96boards/meta-96boards
+# realsense
+git clone -b zeus https://github.com/IntelRealSense/meta-intel-realsense.git
+# Yocto zeus meta raspeberry
 git clone -b zeus  git://git.yoctoproject.org/meta-raspberrypi
 . ./oe-init-build-env rpi3-build
 bitbake  core-image-minimal
